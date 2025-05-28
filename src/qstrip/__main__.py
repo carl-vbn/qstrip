d5bb838a6f8d6614d574b9c1bcf7075a187a1651
Carl von Bonin <carl.vonbonin@gmail.com>
2025-05-28 16:48:03 -0400

    Code style

--- src/qstrip/__main__.py
+++ src/qstrip/__main__.py
@@ -6,9 +6,11 @@ from . import strip_markdown
 def main():
     parser = argparse.ArgumentParser(description="Strip markdown")
     parser.add_argument("-i", "--input", type=str,
-                help="Input file to strip markdown from. Defaults to stdin.")
+                        help="Input file to strip markdown from. "
+                        "Defaults to stdin.")
     parser.add_argument("-o", "--output", type=str,
-                help="Output file to write the stripped text to. Defaults to stdout.")
+                        help="Output file to write the stripped "
+                        "text to. Defaults to stdout.")
     args = parser.parse_args()
 
     if args.input:
@@ -24,6 +26,7 @@ def main():
             f.write(stripped_text)
     else:
         sys.stdout.write(stripped_text)
+
 
 if __name__ == "__main__":
     main()
